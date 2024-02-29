39dc6d777ea7d5fe03ae097ca5e19f98e1fdda96
Matheus <progammerantunes@gmail.com>
2024-02-28 21:38:24 -0300

    aula

---
+++ nomes.py
@@ -0,0 +1,22 @@
+# import requests, json
+
+# URL = f'https://servicodados.ibge.gov.br/api/v2/censos/nomes/matheus'
+
+# response = requests.get(URL)
+
+# data = response.json()
+
+# res = data[0]['res']
+
+# for item in res:
+#     print(f'Periodo: {item["periodo"]} e Frequencia:{item["frequencia"]}')
+
+# # print('O nome Matheus existiu: ')
+# print(data[0]['res'][0])
+# print(data[0]['res'][1])
+# print(data[0]['res'][2])
+# print(data[0]['res'][3])
+# print(data[0]['res'][4])
+# print(data[0]['res'][5])
+# print(data[0]['res'][6])
+
---
+++ teste.py
@@ -0,0 +1,18 @@
+# # frutas = [
+# #     {'nome' : 'Uva', 'peso' : '200g'},
+# #     {'nome' : 'Jambo', 'peso' : '100g'},
+# #     {'nome' : 'Laranja', 'peso' : '500g'}
+# # ]
+
+# # print(frutas[1]['nome'])
+# # print(frutas[2]['peso'])
+
+# import requests
+
+# url = 'https://jsonplaceholder.typicode.com/todos/1'
+
+# response = requests.get(url)
+
+# data = response.json()
+
+# print(data['title'])
